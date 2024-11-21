97416adc13f91eb3fc4eda66e935aa13348d0120
rodermund <yannick.rodermund@web.de>
2024-11-21 12:08:00 +0100

    Custom JS

---
+++ README.md
@@ -0,0 +1,4 @@
+# NYT Studio Shopify Theme
+
+## Push changes
+`shopify theme push --store=3c7f75-2e -a -l`
---
+++ assets/custom.js
@@ -0,0 +1,19 @@
+// const listImg = document.querySelectorAll('.media.media--hover-effect>img+img')
+// let delay = 0;
+// listImg.forEach(img => {
+//   setTimeout(() => {
+//     setInterval(() => {
+//       img.style.opacity = 0
+
+//       setTimeout(() => {
+
+//         img.style.opacity = 1
+
+//       }, 4000);
+
+//     }, 8000);
+//   }, delay);
+
+
+//   delay += 2000;
+// });
